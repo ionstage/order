5229cce3fc45ca33fb814d5967e48169a01ebf18
nasitra <nasitra@users.noreply.github.com>
2015-11-20 19:03:12 +0900

    Enable to parse 'bind' command

--- js/models/command.js
+++ js/models/command.js
@@ -8,6 +8,18 @@
     this.moduleName = moduleName;
   };
 
+  command.Bind = function(sourceVariableName, sourceMemberName, targetVariableName, targetMemberName) {
+    this.source = {
+      variableName: sourceVariableName,
+      memberName: sourceMemberName
+    };
+
+    this.target = {
+      variableName: targetVariableName,
+      memberName: targetMemberName
+    };
+  };
+
   command.parseLine = function(line) {
     line = line.split('#')[0].trim();
 
@@ -17,6 +29,16 @@
     var m = line.match(/^([^:]+):([^:]+)$/);
     if (m)
       return [new command.Declare(m[1], m[2])];
+
+    var substrings = line.split('->');
+    if (substrings.length === 2) {
+      var source = substrings[0].split('.');
+      var target = substrings[1].split('.');
+      if (source.length === 2 && target.length === 2)
+        return [new command.Bind(source[0].trim(), source[1].trim(), target[0].trim(), target[1].trim())];
+    }
+
+    return [];
   };
 
   if (typeof module !== 'undefined' && module.exports)
--- test/coco-test.js
+++ test/coco-test.js
@@ -21,5 +21,24 @@ describe('command', function() {
       assert.equal(cmd.variableName, 'x');
       assert.equal(cmd.moduleName, 'Module');
     });
+
+    it('bind', function() {
+      var cmd = command.parseLine('x.member0 -> y.member1')[0];
+      assert(cmd instanceof command.Bind);
+      assert.equal(cmd.source.variableName, 'x');
+      assert.equal(cmd.source.memberName, 'member0');
+      assert.equal(cmd.target.variableName, 'y');
+      assert.equal(cmd.target.memberName, 'member1');
+
+      // without whitespace
+      assert.deepEqual(command.parseLine('x.member0->y.member1')[0], cmd);
+      assert.deepEqual(command.parseLine('x.member0 ->y.member1')[0], cmd);
+      assert.deepEqual(command.parseLine('x.member0-> y.member1')[0], cmd);
+
+      // invalid
+      assert.equal(command.parseLine('x -> y').length, 0);
+      assert.equal(command.parseLine('x.member0 -> y').length, 0);
+      assert.equal(command.parseLine('x.member0 -> y.member1.member2').length, 0);
+    });
   });
 });
