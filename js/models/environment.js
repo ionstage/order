091a1d67b3a8ca511d13d8d211380a40d99ec429
nasitra <nasitra@users.noreply.github.com>
2015-11-29 20:51:43 +0900

    Enable to execute 'declare' command

--- js/models/environment.js
+++ js/models/environment.js
@@ -2,17 +2,28 @@
   'use strict';
   var command = app.command || require('./command.js');
 
-  var Environment = function() {};
+  var Environment = function() {
+    this.variables = [];
+  };
 
   Environment.prototype.exec = function(s) {
     return new Promise(function(resolve, reject) {
       var cmd = command.parseStatement(s);
 
-      if (cmd instanceof command.Noop)
+      if (cmd instanceof command.Noop) {
         resolve();
-      else
+      } else if (cmd instanceof command.Declare) {
+        this.moduleFactory(cmd.moduleName).then(function(module) {
+          this.variables.push({
+            name: cmd.variableName,
+            module: module
+          });
+          resolve();
+        }.bind(this), reject);
+      } else {
         reject();
-    });
+      }
+    }.bind(this));
   };
 
   if (typeof module !== 'undefined' && module.exports)
--- test/environment-test.js
+++ test/environment-test.js
@@ -7,5 +7,24 @@ describe('environment', function() {
       var env = new Environment();
       return env.exec(':noop');
     });
+
+    it(':declare x Module', function() {
+      var env = new Environment();
+      var dummy = {};
+
+      env.moduleFactory = function(moduleName) {
+        return new Promise(function(resolve) {
+          dummy.name = moduleName;
+          resolve(dummy);
+        });
+      };
+
+      return env.exec(':declare x Module').then(function() {
+        var v = env.variables[0];
+        assert.equal(v.name, 'x');
+        assert.equal(v.module, dummy);
+        assert.equal(v.module.name, 'Module');
+      });
+    });
   });
 });
