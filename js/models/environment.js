de63beb8cfe77fc8bcc2376f5419cba27b73a614
nasitra <nasitra@users.noreply.github.com>
2015-12-02 08:06:03 +0900

    Refactor 'Environment#exec' method

--- js/models/environment.js
+++ js/models/environment.js
@@ -14,20 +14,31 @@
     return new Promise(function(resolve, reject) {
       var cmd = command.parseStatement(command.expandAbbreviation(s));
 
-      if (cmd instanceof command.Noop) {
-        resolve();
-      } else if (cmd instanceof command.Declare) {
-        this.moduleFactory(cmd.moduleName).then(function(props) {
-          this.variables.push({
-            name: cmd.variableName,
-            module: new Module(props)
-          });
-          resolve();
-        }.bind(this), reject);
-      } else {
+      var name = command.names().map(function(name) {
+        return name.charAt(0).toUpperCase() + name.slice(1);
+      }).filter(function(name) {
+        return (cmd instanceof command[name]);
+      })[0];
+
+      if (name)
+        this['exec' + name](cmd, resolve, reject);
+      else
         reject();
-      }
     }.bind(this));
+  };
+
+  Environment.prototype.execNoop = function(cmd, resolve, reject) {
+    resolve();
+  };
+
+  Environment.prototype.execDeclare = function(cmd, resolve, reject) {
+    this.moduleFactory(cmd.moduleName).then(function(props) {
+      this.variables.push({
+        name: cmd.variableName,
+        module: new Module(props)
+      });
+      resolve();
+    }.bind(this), reject);
   };
 
   if (typeof module !== 'undefined' && module.exports)
