24dd53a4f7b0f4aa2c485a29bf50c25d3e4630e1
nasitra <nasitra@users.noreply.github.com>
2015-12-12 10:26:39 +0900

    Enable loading variable-component

--- js/components/variable-component.js
+++ js/components/variable-component.js
@@ -2,11 +2,26 @@
   'use strict';
 
   var helper = app.helper || require('../helper.js');
+  var dom = app.dom || require('../dom.js');
   var Component = app.Component || require('./component.js');
 
-  var VariableComponent = helper.inherits(function() {
+  var VariableComponent = helper.inherits(function(props) {
     VariableComponent.super_.call(this);
+    this.name = this.prop(props.name);
+    this.moduleName = this.prop(props.moduleName);
+    this.element = this.prop(null);
+    this.parentElement = this.prop(props.parentElement);
   }, Component);
+
+  VariableComponent.load = function(props) {
+    var component = new VariableComponent(props);
+    return new Promise(function(resolve) {
+      var element = dom.el('<div>');
+      dom.append(component.parentElement(), element);
+      component.element(element);
+      resolve(component);
+    });
+  };
 
   if (typeof module !== 'undefined' && module.exports)
     module.exports = VariableComponent;
--- js/dom.js
+++ js/dom.js
@@ -7,6 +7,17 @@
     return (typeof document === 'undefined');
   };
 
+  dom.el = function(selector) {
+    if (selector.charAt(0) === '<') {
+      selector = selector.match(/<(.+)>/)[1];
+      return document.createElement(selector);
+    }
+  };
+
+  dom.append = function(parent, el) {
+    parent.appendChild(el);
+  };
+
   dom.animate = function(callback) {
     return window.requestAnimationFrame(callback);
   };
