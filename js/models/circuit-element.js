d277a8b494aa0bc2075d18e34f990ce89748c4e0
nasitra <nasitra@users.noreply.github.com>
2015-12-19 19:53:53 +0900

    Wrap functions from 'circuit' by 'circuit element'

--- js/models/circuit-element.js
+++ js/models/circuit-element.js
@@ -1,7 +1,42 @@
 (function(app) {
   'use strict';
 
-  var CircuitElement = function() {};
+  var circuit = require('circuit');
+
+  var CircuitProp = function(props) {
+    this.name = props.name;
+    this.callee = circuit.prop();
+  };
+
+  var CircuitEvent = function(props) {
+    this.name = props.name;
+    this.callee = circuit.event();
+  };
+
+  var CircuitElement = function(members) {
+    this.members = members.map(function(member) {
+      // wrap object into array
+      member = [].concat(member);
+
+      var name = member[0];
+      var CircuitType = (name.indexOf('on') === 0) ? CircuitEvent : CircuitProp;
+
+      return new CircuitType({
+        name: name
+      });
+    });
+  };
+
+  CircuitElement.prototype.get = function(name) {
+    var member = this.members.filter(function(member) {
+      return member.name === name;
+    })[0];
+
+    if (!member)
+      return null;
+
+    return member.callee;
+  };
 
   if (typeof module !== 'undefined' && module.exports)
     module.exports = CircuitElement;
---
+++ test/circuit-element-test.js
@@ -0,0 +1,15 @@
+var assert = require('assert');
+var CircuitElement = require('../js/models/circuit-element.js');
+
+describe('CircuitElement', function() {
+  it('#get', function() {
+    var cel = new CircuitElement([
+      'prop',
+      'onevent'
+    ]);
+
+    assert.equal(typeof cel.get('prop'), 'function');
+    assert.equal(typeof cel.get('onevent'), 'function');
+    assert.strictEqual(cel.get('event'), null);
+  });
+});
