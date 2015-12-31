93dc489aa9bac460dc69cdf3b64665d9d41cd453
nasitra <nasitra@users.noreply.github.com>
2015-12-31 18:12:55 +0900

    Add factory method to create a circuit element in 'content component'

--- js/components/content-component.js
+++ js/components/content-component.js
@@ -3,10 +3,24 @@
 
   var helper = app.helper || require('../helper.js');
   var Component = app.Component || require('./component.js');
+  var VariableComponent = app.VariableComponent || require('./variable-component.js');
 
-  var ContentComponent = helper.inherits(function() {
+  var ContentComponent = helper.inherits(function(props) {
     ContentComponent.super_.call(this);
+    this.variables = this.prop([]);
+    this.element = this.prop(props.element);
   }, Component);
+
+  ContentComponent.prototype.circuitElementFactory = function(props) {
+    return VariableComponent.load({
+      name: props.variableName,
+      moduleName: props.moduleName,
+      parentElement: this.element()
+    }).then(function(component) {
+      this.variables().push(component);
+      return component.circuitElement();
+    }.bind(this));
+  };
 
   if (typeof module !== 'undefined' && module.exports)
     module.exports = ContentComponent;
