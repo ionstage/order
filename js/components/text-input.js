8302647c3b3e90c6631241fc96978f18b60c7ba4
nasitra <nasitra@users.noreply.github.com>
2016-01-04 08:27:36 +0900

    Dispatch 'enter' event of text-input component

--- js/components/text-input.js
+++ js/components/text-input.js
@@ -2,11 +2,17 @@
   'use strict';
 
   var helper = app.helper || require('../helper.js');
+  var dom = app.dom || require('../dom.js');
   var Component = app.Component || require('./component.js');
 
   var TextInput = helper.inherits(function(props) {
     TextInput.super_.call(this);
-    this.element = this.prop(props.element);
+
+    var element = props.element;
+    this.element = this.prop(element);
+    this.onenter = props.onenter;
+
+    dom.on(element, 'keydown', TextInput.onkeydown.bind(this));
   }, Component);
 
   TextInput.prototype.text = function(s) {
@@ -16,6 +22,11 @@
       return element.value;
 
     element.value = s;
+  };
+
+  TextInput.onkeydown = function(event) {
+    if (event.which === 13)
+      this.onenter();
   };
 
   if (typeof module !== 'undefined' && module.exports)
