69309b7e096a97faed53affedfc5e879e8a0f6bc
nasitra <nasitra@users.noreply.github.com>
2016-01-03 11:12:19 +0900

    Add accessor of a text to a text-input component

--- js/components/text-input.js
+++ js/components/text-input.js
@@ -4,9 +4,19 @@
   var helper = app.helper || require('../helper.js');
   var Component = app.Component || require('./component.js');
 
-  var TextInput = helper.inherits(function() {
+  var TextInput = helper.inherits(function(props) {
     TextInput.super_.call(this);
+    this.element = this.prop(props.element);
   }, Component);
+
+  TextInput.prototype.text = function(s) {
+    var element = this.element();
+
+    if (typeof s === 'undefined')
+      return element.value;
+
+    element.value = s;
+  };
 
   if (typeof module !== 'undefined' && module.exports)
     module.exports = TextInput;
