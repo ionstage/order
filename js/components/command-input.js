359cdaccbc83c30e00fa611b45e0efe031044336
nasitra <nasitra@users.noreply.github.com>
2018-02-15 21:27:16 +0900

    Modify code for 'history' model of command-input

--- js/components/command-input.js
+++ js/components/command-input.js
@@ -4,46 +4,11 @@
   var jCore = require('jcore');
   var dom = app.dom || require('../dom.js');
 
-  var InputHistory = function() {
-    this.data = [];
-    this.index = 0;
-    this.size = 100;
-  };
-
-  InputHistory.prototype.back = function() {
-    this.index = Math.max(this.index - 1, 0);
-    return this.current();
-  };
-
-  InputHistory.prototype.forward = function() {
-    this.index = Math.min(this.index + 1, this.data.length);
-    return this.current();
-  };
-
-  InputHistory.prototype.push = function(text) {
-    this.data = this.data.slice(-this.size - 1);
-    this.data.push(text);
-    this.index = this.data.length;
-  };
-
-  InputHistory.prototype.current = function() {
-    return this.data[this.index] || '';
-  };
-
-  InputHistory.prototype.get = function() {
-    return this.data;
-  };
-
-  InputHistory.prototype.set = function(data) {
-    this.data = data.slice(-this.size);
-    this.index = this.data.length;
-  };
-
   var CommandInput = jCore.Component.inherits(function(props) {
     this.executor = props.executor;
     this.historyLoader = props.historyLoader;
     this.historySaver = props.historySaver;
-    this.inputHistory = new InputHistory();
+    this.history = new CommandInput.History();
 
     this.loadHistory().then(function() {
       this.focus();
@@ -81,7 +46,7 @@
       this.disabled(true);
       return this.executor(text);
     }.bind(this)).then(function() {
-      this.inputHistory.push(text);
+      this.history.push(text);
       this.saveHistory();
       // clear input text
       this.text('');
@@ -96,19 +61,19 @@
 
   CommandInput.prototype.onup = function(event) {
     event.preventDefault();
-    this.text(this.inputHistory.back());
+    this.text(this.history.back());
   };
 
   CommandInput.prototype.ondown = function(event) {
     event.preventDefault();
-    this.text(this.inputHistory.forward());
+    this.text(this.history.forward());
   };
 
   CommandInput.prototype.loadHistory = function() {
     this.disabled(true);
 
     return this.historyLoader().then(function(data) {
-      this.inputHistory.set(data);
+      this.history.set(data);
       this.disabled(false);
     }.bind(this)).catch(function() {
       this.disabled(false);
@@ -116,7 +81,7 @@
   };
 
   CommandInput.prototype.saveHistory = function() {
-    this.historySaver(this.inputHistory.get());
+    this.historySaver(this.history.get());
   };
 
   CommandInput.keyMap = {
@@ -124,6 +89,45 @@
     38: 'up',
     40: 'down'
   };
+
+  CommandInput.History = (function() {
+    var History = function() {
+      this.data = [];
+      this.index = 0;
+      this.size = 100;
+    };
+
+    History.prototype.back = function() {
+      this.index = Math.max(this.index - 1, 0);
+      return this.current();
+    };
+
+    History.prototype.forward = function() {
+      this.index = Math.min(this.index + 1, this.data.length);
+      return this.current();
+    };
+
+    History.prototype.push = function(text) {
+      this.data = this.data.slice(-this.size - 1);
+      this.data.push(text);
+      this.index = this.data.length;
+    };
+
+    History.prototype.current = function() {
+      return this.data[this.index] || '';
+    };
+
+    History.prototype.get = function() {
+      return this.data;
+    };
+
+    History.prototype.set = function(data) {
+      this.data = data.slice(-this.size);
+      this.index = this.data.length;
+    };
+
+    return History;
+  })();
 
   if (typeof module !== 'undefined' && module.exports)
     module.exports = CommandInput;
