be99f8f55851865e75b281d593be95e5eae886d8
nasitra <nasitra@users.noreply.github.com>
2018-02-16 18:52:32 +0900

    Make 'main' component

--- js/app.js
+++ js/app.js
@@ -1,79 +1,9 @@
 (function(app) {
   'use strict';
 
-  var helper = app.helper || require('./helper.js');
-  var dom = app.dom || require('./dom.js');
-  var Environment = app.Environment || require('./models/environment.js');
-  var CommandInput = app.CommandInput || require('./components/command-input.js');
-  var Content = app.Content || require('./components/content.js');
+  var Main = app.Main || require('./components/main.js');
 
-  var App = function() {
-    helper.bindAll(this);
-
-    this.env = new Environment({
-      circuitElementFactory: this.circuitElementFactory,
-      circuitElementDisposal: this.circuitElementDisposal,
-      scriptLoader: this.scriptLoader,
-      scriptSaver: this.scriptSaver
-    });
-
-    this.commandInput = new CommandInput({
-      element: dom.el('.command-input'),
-      executor: this.executor,
-      historyLoader: this.historyLoader,
-      historySaver: this.historySaver
-    });
-
-    this.content = new Content({
-      element: dom.el('.content')
-    });
-  };
-
-  App.prototype.circuitElementFactory = function(props) {
-    return this.content.loadVariable(props.variableName, props.moduleName).then(function(variable) {
-      return variable.circuitElement();
-    });
-  };
-
-  App.prototype.circuitElementDisposal = function(props) {
-    this.content.deleteVariable(props.variableName);
-  };
-
-  App.prototype.scriptLoader = function(filePath) {
-    if (!filePath)
-      throw new Error('OrderScript runtime error: Invalid script file path');
-
-    return dom.ajax({
-      type: 'GET',
-      url: 'order_scripts/' + filePath
-    });
-  };
-
-  App.prototype.scriptSaver = function(filePath, scriptText) {
-    return dom.openWindow(filePath, '<pre>' + scriptText + '</pre>').catch(function() {
-      throw new Error('OrderScript runtime error: Failed to save script');
-    });
-  };
-
-  App.prototype.executor = function(text) {
-    return this.env.exec(text);
-  };
-
-  App.prototype.historyLoader = function() {
-    return Promise.resolve().then(function() {
-      return dom.load(App.KEY_INPUT_HISTORY, []);
-    });
-  };
-
-  App.prototype.historySaver = function(data) {
-    return Promise.resolve().then(function() {
-      dom.save(App.KEY_INPUT_HISTORY, data);
-    });
-  };
-
-  App.KEY_INPUT_HISTORY = 'order/input-history';
-
-  app.main = new App();
+  app.main = new Main();
 
   if (typeof module !== 'undefined' && module.exports) {
     app.CircuitElement = require('./models/circuit-element.js');
---
+++ js/components/main.js
@@ -0,0 +1,82 @@
+(function(app) {
+  'use strict';
+
+  var jCore = require('jcore');
+  var helper = app.helper || require('../helper.js');
+  var dom = app.dom || require('../dom.js');
+  var Environment = app.Environment || require('../models/environment.js');
+  var CommandInput = app.CommandInput || require('./command-input.js');
+  var Content = app.Content || require('./content.js');
+
+  var Main = jCore.Component.inherits(function() {
+    helper.bindAll(this);
+
+    this.env = new Environment({
+      circuitElementFactory: this.circuitElementFactory,
+      circuitElementDisposal: this.circuitElementDisposal,
+      scriptLoader: this.scriptLoader,
+      scriptSaver: this.scriptSaver
+    });
+
+    this.commandInput = new CommandInput({
+      element: dom.el('.command-input'),
+      executor: this.executor,
+      historyLoader: this.historyLoader,
+      historySaver: this.historySaver
+    });
+
+    this.content = new Content({
+      element: dom.el('.content')
+    });
+  });
+
+  Main.prototype.circuitElementFactory = function(props) {
+    return this.content.loadVariable(props.variableName, props.moduleName).then(function(variable) {
+      return variable.circuitElement();
+    });
+  };
+
+  Main.prototype.circuitElementDisposal = function(props) {
+    this.content.deleteVariable(props.variableName);
+  };
+
+  Main.prototype.scriptLoader = function(filePath) {
+    if (!filePath)
+      throw new Error('OrderScript runtime error: Invalid script file path');
+
+    return dom.ajax({
+      type: 'GET',
+      url: 'order_scripts/' + filePath
+    });
+  };
+
+  Main.prototype.scriptSaver = function(filePath, scriptText) {
+    return dom.openWindow(filePath, '<pre>' + scriptText + '</pre>').catch(function() {
+      throw new Error('OrderScript runtime error: Failed to save script');
+    });
+  };
+
+  Main.prototype.executor = function(text) {
+    return this.env.exec(text);
+  };
+
+  Main.prototype.historyLoader = function() {
+    return Promise.resolve().then(function() {
+      return dom.load(Main.KEY_INPUT_HISTORY, []);
+    });
+  };
+
+  Main.prototype.historySaver = function(data) {
+    return Promise.resolve().then(function() {
+      dom.save(Main.KEY_INPUT_HISTORY, data);
+    });
+  };
+
+  Main.KEY_INPUT_HISTORY = 'order/input-history';
+
+  if (typeof module !== 'undefined' && module.exports) {
+    module.exports = Main;
+  } else {
+    app.Main = Main;
+  }
+})(this.app || (this.app = {}));
