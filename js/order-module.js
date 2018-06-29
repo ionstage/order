b0c0ad26bc2ba487b95c01e9f86d48a26791466c
nasitra <nasitra@users.noreply.github.com>
2018-06-29 20:53:31 +0900

    VERSION 0.2.2

--- js/order-module.js
+++ js/order-module.js
@@ -1,25 +1,10 @@
 (function(window) {
   'use strict';
 
-  var CircuitElement = window.parent.app.CircuitElement;
-
-  var order = {};
-
-  order.Module = function(member) {
-    var self = new CircuitElement(member);
-    var wrapper = {
-      get: CircuitElement.prototype.get.bind(self),
-      getAll: CircuitElement.prototype.getAll.bind(self)
-    };
-
-    return wrapper;
-  };
-
-  order.exports = CircuitElement.empty();
-
-  window.order = order;
-
-  window.addEventListener('load', function() {
-    window.postMessage(window.name, location.protocol + '//' + location.host);
+  Object.defineProperty(window, 'order', {
+    value: Object.create(Object.prototype, {
+      Module: { value: window.parent.OrderModule },
+      exports: { value: null, writable: true },
+    }),
   });
 })(this);
